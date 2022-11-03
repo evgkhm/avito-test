fb5f1102800b12e9c498b78f1e70e1ba833b12e1
Evgenii <evgkhm@mail.ru>
2022-11-03 13:47:18 +0300

    docker files for up and down added

---
+++ db/migrations/000001_create_items_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS usr(
+  id INTEGER PRIMARY KEY,
+  balance DECIMAL,
+  name VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS reservation(
+  id INTEGER PRIMARY KEY,
+  id_service INTEGER,
+  id_order INTEGER,
+  cost DECIMAL
+);
+
+CREATE TABLE IF NOT EXISTS revenue(
+  id INTEGER PRIMARY KEY,
+  id_service INTEGER,
+  id_order INTEGER,
+  cost DECIMAL
+);
